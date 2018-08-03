cb8f18dbaa64e8dae356bd382ae34f771f7d3578
Joel Bond <joel.bond@partialdev.com>
2018-08-03 12:43:07 -0600

    Meep

--- bootstrap.php
+++ bootstrap.php
@@ -16,9 +16,9 @@ use Flarum\Extend;
 use Illuminate\Contracts\Events\Dispatcher;
 return [
     (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/extension.js')
+        ->js(__DIR__.'/js/dist/forum.js')
         ->css(__DIR__.'/resources/less/forum.less'),
     (new Extend\Frontend('admin'))
-        ->js(__DIR__.'/js/dist/extension.js')
+        ->js(__DIR__.'/js/dist/admin.js')
         ->css(__DIR__.'/resources/less/admin.less')
 ];
