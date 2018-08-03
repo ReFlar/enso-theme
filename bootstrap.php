71722e43bd382f5450322ee2f05920b0635d2ec5
Joel Bond <joel.bond@partialdev.com>
2018-08-03 12:17:12 -0600

    It's initializing now!

--- bootstrap.php
+++ bootstrap.php
@@ -1,17 +1,24 @@
 <?php
+
 /**
- *  This file is part of reflar/reactions
+ *  This file is part of reflar/reactions.
  *
  *  Copyright (c) ReFlar.
+ *
+ *  http://reflar.io
  *
  *  For the full copyright and license information, please view the license.md
  *  file that was distributed with this source code.
  */
+
 namespace Reflar\Enso;
 use Flarum\Extend;
 use Illuminate\Contracts\Events\Dispatcher;
 return [
-    function (Dispatcher $events) {
-        $events->subscribe(Listeners\AddClientAssets::class);
-    },
+    (new Extend\Frontend('forum'))
+        ->js(__DIR__.'/js/dist/extension.js')
+        ->css(__DIR__.'/resources/less/forum.less'),
+    (new Extend\Frontend('admin'))
+        ->js(__DIR__.'/js/dist/extension.js')
+        ->css(__DIR__.'/resources/less/admin.less')
 ];
