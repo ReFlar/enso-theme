c0940e6ec8b89a3a07dee78bace6cc91825fa1b8
Joel Bond <joel.bond@partialdev.com>
2018-08-03 12:07:20 -0600

    Progress

--- bootstrap.php
+++ bootstrap.php
@@ -1,19 +1,19 @@
 <?php
-
 /**
- *  This file is part of reflar/enso-theme.
+ *  This file is part of reflar/reactions
  *
- *  Copyright (c) 2018 .
+ *  Copyright (c) ReFlar.
  *
- *
- *  For the full copyright and license information, please view the LICENSE.md
+ *  For the full copyright and license information, please view the license.md
  *  file that was distributed with this source code.
  */
-
 namespace Reflar\Enso;
-
+use Flarum\Extend;
 use Illuminate\Contracts\Events\Dispatcher;
-
-return function (Dispatcher $events) {
-    $events->subscribe(Listeners\AddClientAssets::class);
-};
+return [
+    function (Dispatcher $events) {
+        $events->subscribe(Listener\AddPostReactionsRelationship::class);
+        $events->subscribe(Listener\SaveReactionsToDatabase::class);
+        $events->subscribe(Listener\SendNotificationWhenPostIsReacted::class);
+    },
+];
