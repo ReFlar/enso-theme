18278063a6fdcb09e64ce53f906aea8896dc36ea
adamkearn <adamkearn17@gmail.com>
2018-12-30 17:17:58 +0000

    Adam's Update

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -2,8 +2,27 @@ import app from 'flarum/app';
 import DiscussionListItem from 'flarum/components/DiscussionListItem';
 import { extend } from "flarum/extend";
 
+function genUnreadIdentifier(){
+  var span = document.createElement("span");
+  var node = document.createTextNode("NEW");
+  span.appendChild(node);
+}
+
 app.initializers.add('reflar/enso-theme', () => {
   extend(DiscussionListItem.prototype, "view", vnode => {
-    vnode.children.push(<span>iH</span>);
-  });
-});
+    // Inport the CSS file into the DOM for the new elements to use.
+    (new Extend\Frontend('forum'))
+      ->css(__DIR__.'unreadpost-style.css')
+
+    // Find all the elements in the DOM that have the unread class
+    // and add them to an array.  Then iterate over appending
+    // our custom element to the post.
+    var elem = document.getElementsByClassName("unread");
+    for (var i = 0; i < elem.length; i++) {
+       var identifier = genUnreadIdentifier();
+       elem[i].children[2].children[0].appendChild(identifier);
+    }
+    console.log(elem);
+   }
+  )}
+);
